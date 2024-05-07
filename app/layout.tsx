553e317158965bb5937202cd9962db2be9263ee1
Marco Ciampini <marco.ciampo@gmail.com>
2024-05-07 10:32:36 +0200

    Refactor layout, style top nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,60 +27,81 @@ export default async function RootLayout({
   const supabase = createClient();
   const { data, error } = await supabase.auth.getUser();
 
+  const isLoggedIn = !error && data?.user;
+
+  // ERROR missing sub claim
+  // see https://github.com/supabase/supabase-js/issues/992
+
   return (
     <html lang="en">
-      <body className="font-sans min-h-dvh">
-        <nav>
-          <ul className="border-b border-b-black py-2 px-2 flex">
-            <li>
-              <Link href="/" className="flex items-center gap-2 text-sm">
-                <Image
-                  src="/logo.svg"
-                  alt="Expense Manager logo"
-                  className="dark:invert"
-                  width={24}
-                  height={24}
-                  priority
-                />
-                Expense manager
-              </Link>
-            </li>
-            <li>
-              <Link href="/report" className="flex items-center gap-2 text-sm">
-                Report
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/expense/new"
-                className="flex items-center gap-2 text-sm"
-              >
-                Add expense
-              </Link>
-            </li>
-
-            <li className="ml-auto">
-              {error || !data?.user ? (
-                <>
-                  <Link href={'/login'} className="text-sm underline">
-                    Log In
-                  </Link>{' '}
-                  or{' '}
-                  <Link href={'/signup'} className="text-sm underline">
-                    Sign Up
+      <body className="font-sans">
+        {!isLoggedIn ? (
+          <main className="min-h-dvh">{children}</main>
+        ) : (
+          <>
+            <nav className="fixed w-full top-0">
+              <ul className="h-12 border-b border-b-blue-900 px-2 flex items-stretch bg-blue-800 text-white">
+                <li className="flex items-stretch mr-auto">
+                  <Link
+                    href="/"
+                    className="-ml-4 px-4 flex gap-2 items-center text-base underline-offset-2 hover:bg-blue-700 hover:underline focus:bg-blue-700 focus:underline focus:outline-none"
+                  >
+                    <Image
+                      src="/logo.svg"
+                      className="invert"
+                      alt="Expense Manager logo"
+                      width={32}
+                      height={32}
+                      priority
+                    />
+                    Expense manager
                   </Link>
-                </>
-              ) : (
-                <form>
-                  <button className="text-sm underline" formAction={logout}>
-                    Logout
+                </li>
+                <li className="flex items-stretch">
+                  <Link
+                    href="/report"
+                    className="px-4 flex items-center text-base underline-offset-2 hover:bg-blue-700 hover:underline focus:bg-blue-700 focus:underline focus:outline-none"
+                  >
+                    Report
+                  </Link>
+                </li>
+                <li className="flex items-stretch">
+                  <Link
+                    href="/expense/new"
+                    className="px-4 flex items-center text-base underline-offset-2 hover:bg-blue-700 hover:underline focus:bg-blue-700 focus:underline focus:outline-none"
+                  >
+                    Add expense
+                  </Link>
+                </li>
+                <li className="flex items-stretch">
+                  <button
+                    type="button"
+                    className="-mr-2 px-4 flex items-center text-base underline-offset-2 hover:bg-blue-700 hover:underline focus:bg-blue-700 focus:underline focus:outline-none"
+                    // @ts-expect-error
+                    popovertarget="popover-profile"
+                  >
+                    Profile
                   </button>
-                </form>
-              )}
-            </li>
-          </ul>
-        </nav>
-        {children}
+                </li>
+              </ul>
+            </nav>
+            <main className="pt-12 min-h-dvh">{children}</main>
+            <div
+              id="popover-profile"
+              className="fixed inset-auto top-12 right-0 p-3 bg-white text-blue-800"
+              // @ts-expect-error
+              popover=""
+            >
+              <p>{data.user.email}</p>
+
+              <form>
+                <button className="text-base underline" formAction={logout}>
+                  Logout
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -10,29 +10,29 @@ export default async function Home() {
 
   if (error || !data?.user) {
     return (
-      <main className="min-h-dvh flex flex-col items-center justify-center gap-4">
+      <>
         <h1 className="text-4xl font-thin">Expense manager</h1>
-        <p className="text-sm">
-          <Link href={'/signup'} className="underline">
-            Sign Up
-          </Link>{' '}
-          or{' '}
-          <Link href={'/login'} className="underline">
-            Log In
-          </Link>
-        </p>
-        {error ? <p>{error.message}</p> : null}
-      </main>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <p className="text-sm">
+            <Link href={'/signup'} className="underline">
+              Sign Up
+            </Link>{' '}
+            or{' '}
+            <Link href={'/login'} className="underline">
+              Log In
+            </Link>
+          </p>
+          {error ? <p>{error.message}</p> : null}
+        </div>
+      </>
     );
   }
 
   return (
-    <main className="min-h-dvh">
+    <>
       <h1>Dashboard</h1>
-      <p>Hello {data.user.email}</p>
-      <h2>Expenses</h2>
       {/* TODO: use suspense */}
       <ExpensesList userId={data.user.id} />
-    </main>
+    </>
   );
 }
