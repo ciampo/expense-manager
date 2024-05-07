250a6352600289dd1992c6c6228ff4569c39abc6
Marco Ciampini <marco.ciampo@gmail.com>
2024-05-07 11:19:32 +0200

    Refactor home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { createClient } from '@/utils/supabase/server';
 
 import Link from 'next/link';
+import Image from 'next/image';
 
 import ExpensesList from './expense/expenses-list';
 
@@ -10,21 +11,30 @@ export default async function Home() {
 
   if (error || !data?.user) {
     return (
-      <>
-        <h1 className="text-4xl font-thin">Expense manager</h1>
+      <div className="min-h-dvh flex flex-col items-center gap-8 justify-center">
+        <h1 className="text-5xl flex flex-col items-center gap-2">
+          <Image src="/logo.svg" alt="" width={96} height={96} priority />
+          Expense manager
+        </h1>
         <div className="flex flex-col items-center justify-center gap-4">
-          <p className="text-sm">
-            <Link href={'/signup'} className="underline">
+          <p className="text-base flex gap-4 items-center">
+            <Link
+              href={'/signup'}
+              className="px-4 py-2 bg-blue-800 text-white underline-offset-2 hover:bg-blue-700 hover:underline focus:bg-blue-700 focus:underline"
+            >
               Sign Up
-            </Link>{' '}
-            or{' '}
-            <Link href={'/login'} className="underline">
+            </Link>
+            <span>or</span>
+            <Link
+              href={'/login'}
+              className="px-4 py-2 bg-blue-800 text-white underline-offset-2 hover:bg-blue-700 hover:underline focus:bg-blue-700 focus:underline"
+            >
               Log In
             </Link>
           </p>
           {error ? <p>{error.message}</p> : null}
         </div>
-      </>
+      </div>
     );
   }
 
