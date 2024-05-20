a368463198d9098aed62157a26177a9988511d2c
Marco Ciampini <marco.ciampo@gmail.com>
2024-05-20 16:21:38 +0200

    Style add expense page

--- app/expense/new/page.tsx
+++ app/expense/new/page.tsx
@@ -1,12 +1,53 @@
 'use client';
 
-import { useActionState } from 'react';
+import { useActionState, useEffect, useRef, useState } from 'react';
 import { useFormStatus } from 'react-dom';
+
+import Image from 'next/image';
+import Link from 'next/link';
 
 import { createExpense } from './actions';
 
 const initialState = {
   message: '',
+};
+
+const urlIsImage = (url: string) =>
+  /\.(gif|jpe?g|tiff?|png|webp|bmp|heic)$/i.test(url);
+
+const AttachmentPreview = ({
+  url,
+  isImage,
+  children,
+  className,
+}: {
+  url?: string;
+  isImage: boolean;
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  if (!url) {
+    return null;
+  }
+
+  return (
+    <div className={`relative w-full ${className}`}>
+      {isImage ? (
+        <Image
+          width={0}
+          height={0}
+          src={url}
+          alt="expense attachment"
+          className="w-full h-auto"
+        />
+      ) : (
+        <Link href={url} target="_blank" rel="noopener noreferrer">
+          Open in new tab (preview not available)
+        </Link>
+      )}
+      {children}
+    </div>
+  );
 };
 
 function SubmitButton({ children }: { children: React.ReactNode }) {
@@ -25,62 +66,163 @@ function SubmitButton({ children }: { children: React.ReactNode }) {
 
 export default function NewExpensePage() {
   const [state, formAction] = useActionState(createExpense, initialState);
+  const [attachmentPreviewUrl, setAttachmentPreviewUrl] = useState({
+    isImage: false,
+    value: '',
+  });
+  const attachmentInputRef = useRef<HTMLInputElement>(null);
+  const [attachmentInputValue, setAttachmentInputValue] = useState('');
+
+  // update attachment preview URL every time the expense data updates OR
+  // if the user selects a new attachment
+  useEffect(() => {
+    async function createAttachmentPreview() {
+      // Create a preview URL for the user's selected new attachment
+      if (
+        attachmentInputValue &&
+        attachmentInputRef.current?.files &&
+        attachmentInputRef.current.files.length > 0
+      ) {
+        setAttachmentPreviewUrl({
+          isImage: urlIsImage(attachmentInputRef.current.files[0].name),
+          value: URL.createObjectURL(attachmentInputRef.current.files[0]),
+        });
+        return;
+      }
+
+      // If none of the above scenarios happened, it means that there isn't
+      // any attachment to preview.
+      setAttachmentPreviewUrl({ isImage: false, value: '' });
+    }
+
+    createAttachmentPreview();
+  }, [attachmentInputValue]);
 
   return (
-    <main className="min-h-dvh flex flex-col items-center justify-center gap-12">
-      <h1>Add a new expense</h1>
+    <div className="min-h-dvh flex flex-col items-center justify-center gap-12">
+      <h1 className="text-4xl font-thin">Add a new expense</h1>
+      <div className="w-full max-w-xs">
+        <form
+          className="bg-white shadow-md rounded px-8 py-6 mb-4"
+          action={formAction}
+        >
+          <div className="mb-4">
+            <label
+              className="block text-gray-800 text-sm font-semibold mb-2"
+              htmlFor="date"
+            >
+              Date
+            </label>
+            <input
+              className="shadow appearance-none border border-gray-500 rounded w-full py-2 px-3 text-gray-600 leading-tight focus:outline-none focus:shadow-outline"
+              id="date"
+              name="date"
+              type="date"
+              required
+            />
+          </div>
 
-      <form action={formAction}>
-        <p>
-          <label htmlFor="date">Date</label>
-          <input id="date" name="date" type="date" required />
-        </p>
+          {/* TODO: use datalist to auto-suggest existing merchants? */}
+          <div className="mb-4">
+            <label
+              className="block text-gray-800 text-sm font-semibold mb-2"
+              htmlFor="merchant_name"
+            >
+              Merchant
+            </label>
+            <input
+              className="shadow appearance-none border border-gray-500 rounded w-full py-2 px-3 text-gray-600 leading-tight focus:outline-none focus:shadow-outline"
+              id="merchant_name"
+              name="merchant_name"
+              required
+              placeholder="Merchant name"
+            />
+          </div>
 
-        <p>
-          {/* Pre-populate with existing merchants? */}
-          <label htmlFor="merchant_name">Merchant</label>
-          <input
-            id="merchant_name"
-            name="merchant_name"
-            required
-            placeholder="Merchant name"
-          />
-        </p>
+          <div className="mb-4">
+            <label
+              className="block text-gray-800 text-sm font-semibold mb-2"
+              htmlFor="amount"
+            >
+              Amount
+            </label>
+            <input
+              className="shadow appearance-none border border-gray-500 rounded w-full py-2 px-3 text-gray-600 leading-tight focus:outline-none focus:shadow-outline"
+              type="number"
+              min="0.01"
+              step="0.01"
+              id="amount"
+              name="amount"
+              required
+              placeholder="Expense amount"
+            />
+          </div>
 
-        <p>
-          <label htmlFor="amount">Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            id="amount"
-            name="amount"
-            required
-            placeholder="Expense amount"
-          />
-        </p>
-
-        <p>
           {/* Allow a new category + existing ones? */}
-          <label htmlFor="category">Category</label>
-          <select name="category" id="category" required>
-            <option value="">Expense category</option>
-            {['coworking', 'test'].map((cat) => (
-              <option key={cat} value={cat}>
-                {cat}
-              </option>
-            ))}
-          </select>
-        </p>
+          <div className="mb-4">
+            {/* Store existing cats to an array */}
+            <label
+              className="block text-gray-800 text-sm font-semibold mb-2"
+              htmlFor="category"
+            >
+              Category
+            </label>
+            <select
+              className="shadow border border-gray-500 rounded w-full py-2 px-3 text-gray-600 leading-tight focus:outline-none focus:shadow-outline"
+              name="category"
+              id="category"
+              required
+            >
+              <option value="">Expense category</option>
+              {['coworking', 'test'].map((cat) => (
+                <option key={cat} value={cat}>
+                  {cat}
+                </option>
+              ))}
+            </select>
+          </div>
 
-        <p>
-          <label htmlFor="attachment">Attachment</label>
-          <input name="attachment" id="attachment" type="file" />
-        </p>
+          <div className="mb-6">
+            <label
+              className="block text-gray-800 text-sm font-semibold mb-2"
+              htmlFor="attachment"
+            >
+              Attachment
+            </label>
+            <input
+              className="shadow appearance-none border border-gray-500 rounded w-full py-2 px-3 text-gray-600 leading-tight focus:outline-none focus:shadow-outline"
+              name="attachment"
+              id="attachment"
+              type="file"
+              ref={attachmentInputRef}
+              onChange={(e) => setAttachmentInputValue(e.target.value)}
+            />
+            <AttachmentPreview
+              url={attachmentPreviewUrl.value}
+              isImage={attachmentPreviewUrl.isImage}
+              className="mt-2"
+            >
+              <button
+                onClick={() => {
+                  if (attachmentInputRef.current) {
+                    attachmentInputRef.current.value = '';
+                    setAttachmentInputValue('');
+                  }
+                }}
+                type="button"
+                aria-label="Remove attachment"
+                className="absolute right-0 top-0 flex items-center justify-center bg-white border border-black border-solid w-8 h-8"
+                disabled={!attachmentInputValue}
+              >
+                X
+              </button>
+            </AttachmentPreview>
+          </div>
 
-        <SubmitButton>Save</SubmitButton>
-        <p aria-live="polite">{state?.message}</p>
-      </form>
-    </main>
+          <SubmitButton>Save</SubmitButton>
+          <p aria-live="polite">{state?.message}</p>
+        </form>
+      </div>
+    </div>
   );
 }
