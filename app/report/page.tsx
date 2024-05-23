18677421ce68d61c9e510165eb403db1225d4c8c
Marco Ciampini <marco.ciampo@gmail.com>
2024-05-23 11:20:24 +0200

    Refactor report page: move to server side, reduce DB calls, improve UI

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -1,18 +1,11 @@
-'use client';
-import { ChangeEventHandler, useEffect, useState } from 'react';
+import { createClient } from '@/utils/supabase/server';
 
-import { createClient } from '@/utils/supabase/client';
-
-import type { Database } from '@/utils/supabase/database.types';
-
-function slugify(str: string) {
-  return str
-    .replace(/^\s+|\s+$/g, '') // trim leading/trailing white space
-    .toLowerCase() // convert string to lowercase
-    .replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
-    .replace(/\s+/g, '-') // replace spaces with hyphens
-    .replace(/-+/g, '-'); // remove consecutive hyphens
-}
+// TODO:
+// 2. Allow downloading attachments:
+//   - Download multiple via set timeout (https://hyunbinseo.medium.com/download-multiple-files-with-javascript-and-anchor-element-20f89f500ab2)
+//   - Download multiple with multi-download package (https://github.com/sindresorhus/multi-download)
+//   - Create zip on the fly (https://stackoverflow.com/questions/56244902/how-in-js-to-download-more-than-10-files-in-browser-including-firefox)
+//   - Produce bash script (current solution)
 
 const MONTH_NAMES = [
   'January',
@@ -29,117 +22,80 @@ const MONTH_NAMES = [
   'December',
 ];
 
-export default function Home() {
+export default async function ReportPage() {
   const supabase = createClient();
 
-  const [, setError] = useState();
-  const [userId, setUserId] = useState<string>();
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
 
-  const [allExpensesByMonth, setAllExpensesByMonth] = useState<
-    Array<Database['public']['Views']['how_many_expenses_by_month']['Row']>
-  >([]);
+  if (!user?.id || userError) {
+    throw new Error(`Error while retrieving user info. ${userError?.message}`);
+  }
 
-  const [selectedMonth, setSelectedMonth] = useState<string>();
+  const { data: allExpensesByMonthData, error: allExpensesFetchError } =
+    await supabase
+      .from('how_many_expenses_by_month')
+      .select('*')
+      .eq('user_id', user.id);
 
-  const [selectedMonthReportData, setSelectedMonthReportData] = useState<
-    Array<
-      Database['public']['Views']['expenses_summary_by_date_and_category']['Row']
-    >
-  >([]);
+  if (allExpensesFetchError) {
+    throw new Error(
+      `Error while retrieving expense info. ${allExpensesFetchError.message}`
+    );
+  }
 
-  const [dataAsCSV, setDataAsCSV] = useState<string>();
-
-  const [selectedMonthAttachments, setSelectedMonthAttachments] = useState<
-    Array<{ url: string; filename: string }>
-  >([]);
-
-  // fetch and store user info
-  useEffect(() => {
-    async function fetchUserId() {
-      const {
-        data: { user },
-        error,
-      } = await supabase.auth.getUser();
-
-      if (!user?.id || error) {
-        setError(() => {
-          throw new Error('Error while fetching user information');
-        });
-      } else {
-        setUserId(user.id);
-      }
-    }
-
-    fetchUserId();
-  }, [supabase]);
-
-  // Fetch the expense by month
-  useEffect(() => {
-    async function fetchExpenseData() {
-      if (!userId) {
-        return;
-      }
-
-      const { data, error } = await supabase
-        .from('how_many_expenses_by_month')
-        .select('*')
-        .eq('user_id', userId);
-
-      if (error) {
-        // TODO: handle error
-        console.error(error.message);
-      }
-
-      setAllExpensesByMonth(data ?? []);
-    }
-
-    fetchExpenseData();
-  }, [supabase, userId]);
-
-  // fetch month report when user makes a month selection
-  const onMonthSelected: ChangeEventHandler<HTMLSelectElement> = async (e) => {
-    const date = new Date(e.target.value);
-
-    const year = date.getFullYear();
-    const month = date.getMonth();
-
-    const nextMonthYear = month === 11 ? year + 1 : year;
-    const nextMonthMonth = month === 11 ? 0 : month + 1;
-
-    setSelectedMonth(`${year}-${`${month + 1}`.padStart(2, '0')}`);
-
-    if (!userId) {
-      return;
-    }
-
-    const { data, error } = await supabase
+  const { data: expenseSummaryData, error: expenseSummaryFetchError } =
+    await supabase
       .from('expenses_summary_by_date_and_category')
       .select('*')
-      .eq('user_id', userId)
-      .gte('date', `${year}-${month + 1}-01`)
-      .lt('date', `${nextMonthYear}-${nextMonthMonth + 1}-01`);
+      .eq('user_id', user.id);
 
-    if (error) {
-      // TODO: handle error
-      console.error(error.message);
-      setSelectedMonthReportData([]);
-      return;
-    }
+  if (expenseSummaryFetchError) {
+    throw new Error(
+      `Error while retrieving expense info. ${expenseSummaryFetchError.message}`
+    );
+  }
 
-    if (data && data.length > 0) {
-      setSelectedMonthReportData(data);
-    }
+  type Data = {
+    id: string;
+    dateFormatted: string;
+    count: number;
+    csvFilename: string;
+    csvData: string;
   };
+  const aggregatedData = allExpensesByMonthData.map(
+    ({ count, month: dbStringDate }) => {
+      if (!dbStringDate || !count) {
+        return null;
+      }
 
-  // create CSV report
-  useEffect(() => {
-    if (selectedMonthReportData.length === 0) {
-      setDataAsCSV('');
-      return;
-    }
+      const thisMonthDate = new Date(dbStringDate);
 
-    setDataAsCSV(
-      `data:text/csv;charset=utf-8,giorno,descrizione,aliquota,imponibile,imposta,imponibile,imposta,totale spese documentate\n${selectedMonthReportData
+      const thisMonthYear = thisMonthDate.getFullYear();
+      const thisMonthMonth = thisMonthDate.getMonth();
+
+      const nextMonthYear =
+        thisMonthMonth === 11 ? thisMonthYear + 1 : thisMonthYear;
+      const nextMonthMonth = thisMonthMonth === 11 ? 0 : thisMonthMonth + 1;
+      const nextMonthDate = new Date(
+        `${nextMonthYear}-${nextMonthMonth + 1}-01`
+      );
+
+      const thisMonthDateFormatted = `${MONTH_NAMES[thisMonthMonth]} ${thisMonthYear}`;
+
+      const filteredExpenseData = expenseSummaryData.filter(({ date }) => {
+        if (!date) {
+          return false;
+        }
+
+        const expenseDate = new Date(date);
+
+        return expenseDate >= thisMonthDate && expenseDate < nextMonthDate;
+      });
+
+      const csvData = `data:text/csv;charset=utf-8,${MONTH_NAMES[thisMonthMonth]}\ngiorno,descrizione,aliquota,imponibile,imposta,imponibile,imposta,totale spese documentate\n${filteredExpenseData
         .map(({ date, category, total_amount }) => {
           if (!date || !category || !total_amount) {
             return;
@@ -156,157 +112,50 @@ export default function Home() {
           ].join(',');
         })
         .filter(Boolean)
-        .join('\n')}`
-    );
-  }, [selectedMonthReportData]);
+        .join('\n')}`;
 
-  // get attachment list
-  useEffect(() => {
-    async function fetchedSelectedMonthAttachments() {
-      if (!userId || !selectedMonth) {
-        setSelectedMonthAttachments([]);
-        return;
-      }
-
-      const date = new Date(selectedMonth);
-
-      const year = date.getFullYear();
-      const month = date.getMonth();
-
-      const nextMonthYear = month === 11 ? year + 1 : year;
-      const nextMonthMonth = month === 11 ? 0 : month + 1;
-
-      const { data: expensesWithAttachments, error } = await supabase
-        .from('expenses')
-        .select('*')
-        .eq('user_id', userId)
-        .neq('attachment', null)
-        .gte('date', `${year}-${month + 1}-01`)
-        .lt('date', `${nextMonthYear}-${nextMonthMonth + 1}-01`);
-
-      if (error) {
-        // TODO: handle error
-        console.error(error.message);
-      }
-
-      if (expensesWithAttachments && expensesWithAttachments.length) {
-        // TODO: handle errors
-        Promise.all(
-          expensesWithAttachments.map(
-            (expense) =>
-              new Promise<{ url: string; filename: string }>(
-                async (resolve, reject) => {
-                  if (!expense.attachment) {
-                    reject(undefined);
-                    return;
-                  }
-                  const { data, error } = await supabase.storage
-                    .from('expenses')
-                    .createSignedUrl(expense.attachment, 60);
-
-                  if (error) {
-                    reject(error.message);
-                    return;
-                  }
-
-                  const { extension } =
-                    data.signedUrl.match(/(?<extension>\.\w+)\\?\?/i)?.groups ||
-                    {};
-
-                  console.log(extension);
-
-                  resolve({
-                    url: data.signedUrl,
-                    filename: `${selectedMonth}/${slugify(
-                      `${expense.date}-${expense.category}-${expense.merchant_name}-${expense.id}`
-                    )}${extension ?? ''}`,
-                  });
-                }
-              )
-          )
-        ).then((values) => {
-          setSelectedMonthAttachments(values);
-        });
-      }
+      return {
+        id: dbStringDate,
+        dateFormatted: thisMonthDateFormatted,
+        count,
+        csvFilename: `${thisMonthYear}-${`${thisMonthMonth + 1}`.padStart(2, '0')}-expense-report.csv`,
+        csvData,
+      } as Data;
     }
-
-    fetchedSelectedMonthAttachments();
-  }, [supabase, userId, selectedMonth]);
-
-  console.log(
-    selectedMonthAttachments
-      .map(({ url, filename }, i) => `curl --create-dirs -o ${filename} ${url}`)
-      .join(' && ')
   );
 
   return (
-    <main className="min-h-dvh">
-      <h1>Generate report</h1>
+    <div className="min-h-dvh pt-8 max-w-screen-lg mx-auto">
+      <h1 className="text-blue-800 text-3xl mb-8">Generate reports</h1>
 
-      <select name="expenses_month" onChange={onMonthSelected}>
-        {allExpensesByMonth.length === 0 ? (
-          <option value="">Loading data</option>
-        ) : (
-          <>
-            <option value="">Select a month</option>
-            {allExpensesByMonth.map(({ count, month }) => {
-              if (!month || !count) {
-                return null;
-              }
-
-              const date = new Date(month);
-              const monthFormatted = `${MONTH_NAMES[date.getMonth()]} ${date.getFullYear()}`;
-
-              return (
-                <option key={month} value={month}>
-                  {monthFormatted} [{count} expenses]
-                </option>
-              );
-            })}
-          </>
-        )}
-      </select>
-
-      {selectedMonthReportData.length > 0 && (
-        <>
-          <p>Report</p>
-          <table>
-            <thead>
-              <tr>
-                <th scope="col">Day</th>
-                <th scope="col">Category</th>
-                <th scope="col">Amount</th>
-              </tr>
-            </thead>
-            <tbody>
-              {selectedMonthReportData.map(
-                ({ category, date, total_amount }) => {
-                  if (!date || !category || !total_amount) {
-                    return null;
-                  }
-
-                  return (
-                    <tr key={slugify(`${date}-${category}`)}>
-                      <td>{new Date(date).getDate()}</td>
-                      <td>{category}</td>
-                      <td>{total_amount}</td>
-                    </tr>
-                  );
-                }
-              )}
-            </tbody>
-          </table>
-        </>
+      {aggregatedData.length === 0 ? (
+        <p>No expense data available</p>
+      ) : (
+        <ul className="w-full bg-white shadow-md rounded border border-blue-800">
+          {aggregatedData.map((e, i) =>
+            e === null ? null : (
+              <li
+                className={`border-t first:border-none border-t-slate-400 bg-opacity-30 ${i % 2 === 0 ? 'bg-blue-100' : 'bg-white'} px-4 py-4 flex flex-col gap-2 md:flex-row md:items-center md:justify-between`}
+                key={e.id}
+              >
+                <p className="flex flex-row items-center gap-2 md:flex-col md:items-start">
+                  <span className="font-medium text-lg">{e.dateFormatted}</span>
+                  <span>[{e.count} expenses]</span>
+                </p>
+                <div>
+                  <a
+                    href={encodeURI(e.csvData)}
+                    download={e.csvFilename}
+                    className="inline-block rounded px-4 py-1 bg-white text-blue-700 border border-current underline-offset-2 hover:bg-blue-100 hover:underline focus:bg-blue-100 focus:underline focus:outline-none focus:shadow-outline"
+                  >
+                    Download CSV
+                  </a>
+                </div>
+              </li>
+            )
+          )}
+        </ul>
       )}
-
-      {!!selectedMonth && !!dataAsCSV && (
-        <a
-          href={encodeURI(dataAsCSV)}
-          download={`${selectedMonth}-expense-report.csv`}
-        >
-          Download CSV
-        </a>
-      )}
-    </main>
+    </div>
   );
 }
